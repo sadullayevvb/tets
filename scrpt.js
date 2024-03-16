b62a42da09f8f23552158ad262c7d2621d1e8d50
sadullayevvb <sadullayevvb@gmail.com>
2024-03-16 09:32:35 +0500

    add

---
+++ scrpt.js
@@ -0,0 +1,14 @@
+// let x =Number(prompt('Yoshingizni kriting'));
+// let y =16;
+// if(x>=y){
+//  document.write('You are old')
+// }else{
+//   document.write('You are young')
+// }
+// let test =prompt('What your subject')
+
+// if(test == 'student'){
+//  alert('wow im student too' )
+// }
+
+
